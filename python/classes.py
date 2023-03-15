f17e56920bc71ab2ffc4039da6224777c665dedf
ASUMAN ODABAŞI <103738413+asumanodabasi@users.noreply.github.com>
2023-03-15 10:56:34 +0300

    classes added

---
+++ python/classes.py
@@ -0,0 +1,10 @@
+class Banka:
+
+   def krediBasvur(self):
+    print("Kredi basvurusu yapıldı")
+   def krediHesapla(self):
+    print("Hesaplar yapıldı")
+banka=Banka() #instance
+banka.krediBasvur()
+#this=self
+
