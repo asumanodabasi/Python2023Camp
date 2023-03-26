8b71066eaac1161072e9b3746037dae5f1a24be0
ASUMAN ODABAŞI <103738413+asumanodabasi@users.noreply.github.com>
2023-03-26 17:20:13 +0300

    Delete python/python directory

--- python/python/person.py
+++
@@ -1,9 +0,0 @@
-class Person:
-  def __init__(self,name,lastName):
-    self.name=name
-    self.lastName=lastName
-
-musteri1=Person("Ahmet","Demiroğ")
-musteri2=Person("Kerem","Varış")
-musteri3=Person("İlker","Tural")
-print(musteri1.name)
